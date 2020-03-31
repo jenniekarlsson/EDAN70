57702ac247a17f41c13b9fa44a33849e3a6ff133
Jesper Laurell <jesper.laurell1@gmail.com>
2020-03-31 12:25:04 +0200

    started

---
+++ corona-master/manuscript/about.md
@@ -0,0 +1,1 @@
+manuscript and all files, reuse permitted under GPL_v3 licence
---
+++ tagger.py
@@ -0,0 +1,7 @@
+import sys, os, pathlib
+
+parentpath = str(pathlib.Path(__file__).parent.absolute())
+
+comm_use_subset_100 = parentpath + "/comm_use_subset_100"
+file = open(comm_use_subset_100)
+
