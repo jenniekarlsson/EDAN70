dcab884859ad770da8d38f5f0a415afd84b0c5c8
Jesper Laurell <jesper.laurell1@gmail.com>
2020-04-03 10:13:52 +0200

    removed files

--- tagger.py
+++ tagger.py
@@ -1,19 +1,9 @@
 import sys, os, pathlib, json
 
-parentpath = str(pathlib.Path(__file__).parent.absolute())
-
-metafile = open(parentpath + "/meta_subset_100.csv")
-
-#LOOP START
-
-
 filename = os.listdir(parentpath + "/comm_use_subset_100")[0]
 filepath = parentpath + "/comm_use_subset_100" + "/" + filename
 
-for line in metafile:
-   if filename[:-5] in line:
-        cord_uid = line[:8] # assuming all cord_uid has 8 characters
-        print(cord_uid)
+get_cord_uid(filename)
 
 suffix_counter = 0
 
@@ -26,7 +16,15 @@ with open(filepath) as f:
 with open('title_file_name.txt', 'w') as outfile:
      json.dump(data, outfile)
 
-#LOOP END
 
 
+def get_cord_uid(filename):
+     for line in metafile:
+          if filename[:-5] in line:
+               cord_uid = line[:8]
+               return cord_uid
+
+if __name__ == '__main-__':
+     parentpath = str(pathlib.Path(__file__).parent.absolute())
+     metafile = open(parentpath + "/meta_subset_100.csv")
 
