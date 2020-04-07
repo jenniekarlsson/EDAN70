16946c8b3d7841cfb3bc45784dbdd0bbe0d7b06b
snoboll <jesper.laurell1@gmail.com>
2020-04-07 15:14:05 +0200

    tagger kinda working, but not with correct file generation

--- tagger.py
+++ tagger.py
@@ -14,8 +14,7 @@ def read_article(path):
           abstract_data = [a["text"] for a in d["abstract"]]
           body_text_data = [t["text"] for t in d["body_text"]] #a list of all text sections in article
 
-
-     return [title_data, abstract_data, body_text_data] #
+     return [title_data, abstract_data, body_text_data]
 
 def read_meta(paperid):
      metafile = open("meta_subset_100.csv", "r")
@@ -28,12 +27,38 @@ def read_meta(paperid):
      return [coord_uid, sourcedb, sourceid]
 
 def setup_dicts():
-     covid19_list = [line for line in open ("Supplemental_file2.txt")]
-     sars_list = [line for line in open("Supplemental_file1.txt")]
+     virus_list = [line.strip() for line in open("Supplemental_file1.txt")]
+     disease_list = [line.strip() for line in open ("Supplemental_file2.txt")]
 
-     dicts["covid19"] = covid19_list
-     dicts["sars"] = sars_list
-    
+     dicts["Virus_SARS-CoV-2"] = virus_list
+     dicts["Disease_COVID-19"] = disease_list
+
+def get_longest_match(subsection, d):
+     longestmatch = ""
+     longestbegin = -1
+     longestend = -1
+     for phrase in d:
+          begin = subsection.find(phrase)
+          end = begin + len(phrase)
+          if begin > 0:
+               if is_overlapping(begin, end, longestbegin, longestend):
+                    if len(longestmatch) < len(phrase):
+                         longestmatch = phrase
+                         longestbegin = begin
+                         longestend = begin + len(longestmatch)
+               elif longestmatch == "":
+                    longestmatch = phrase
+                    longestbegin = begin
+                    longestend = begin + len(longestmatch)
+
+     if not longestmatch == "":
+          return longestbegin, longestmatch, longestend
+     else:
+          return None, None, None
+
+def is_overlapping(b1, e1, b2, e2):
+     return not (e2<b1 or e1<b2)
+     
 
 def tag_article(article_path):
      article = read_article(article_path)
@@ -41,22 +66,24 @@ def tag_article(article_path):
      denotated_sections = []
 
      for section in article:
-          denotations = []
           for subsection in section:
+               subsection = subsection.lower()
+               denotations = []
+               longestmatch = ""
                for id in dicts.keys():
-                    for phrase in dicts[id]:
-                         begin = subsection.find("virus")
-                         if begin > 0:#found phrase
-                              end = begin + len(phrase)
-                              info = {"id": id, "span":{"begin":begin, "end":end}, "obj":"?"}
-                              denotations.append(info)
+                    longestbegin, longestmatch, longestend = get_longest_match(subsection, dicts[id])
+                    info = {"id": id, "span":{"begin":longestbegin, "end":longestend}, "obj":"?"}
+                    if not longestmatch == None:
+                         denotations.append(info)
+                         print("found", longestmatch)
+                              
                denotated_sections.append(denotations)
      return denotated_sections
 
-def generate_JSONs(denotated_sections):
-     for filenr in range(20):
-          with open("result.json" + str(filenr), "w") as fp:
-               json.dump(denotated_sections[filenr], fp)
+def generate_JSONs(denotated_sections, j):
+     for i, f in enumerate(denotated_sections):
+          with open("result.json" + str(i) +str(j), "w") as fp:
+               json.dump(f, fp)
 
 
 
@@ -64,12 +91,23 @@ def generate_JSONs(denotated_sections):
 def main():
      subset_path = os.path.abspath("comm_use_subset_100") + "/"
      comm_use_subset_100 = [f for f in listdir(subset_path) if isfile(join(subset_path, f))]
-     fileonepath = subset_path + comm_use_subset_100[0]
+     fileonepath = subset_path + comm_use_subset_100[1]
+     #fileonepath = "/home/jesper/EDAN70/comm_use_subset_100/comm_use_subset_100/04d02a37dcbb17916d2a5c03288cb9b59000ebba.json"
+     
      setup_dicts()
 
-     denot_sections = tag_article(fileonepath)
-     generate_JSONs(denot_sections)
-          
+     denot_sections = tag_article("/home/jesper/EDAN70/fa16032841f11e0924b539d21444915e3bcc9a0e.json")
+     print(denot_sections)
+
+
+     #for j in range(100):
+          #print(comm_use_subset_100[i])
+          #print(read_meta(comm_use_subset_100[i][:-5]))
+
+          #denot_sections = tag_article(subset_path + comm_use_subset_100[j])
+          #print(denot_sections)
+          #generate_JSONs(denot_sections, j)
+
 if __name__ == '__main__':
      main()
      
