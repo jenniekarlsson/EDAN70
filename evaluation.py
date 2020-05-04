2cc9d309e1f287f6c2bbb34eeedd2b05cd853186
Jennie Karlsson <jkarlsson@hawk.iit.edu>
2020-05-04 09:14:51 +0200

    updated evaluation code

--- evaluation.py
+++ evaluation.py
@@ -1,26 +1,51 @@
 
 
-def json_to_dict(json):
+def json_to_dict(json_path):
+    json = open(json_path,"r")
     for line in json:
         dict.append = json.loads(line)
-return dict
+    return dict
 
-def Compare_denot(gold, denot):
 
-    if dict_gold["text"] == denot["text"] &
-    #denotations text & span samma match
+def find_real_entities_retrived(dict_gold, denotation):
+    if dict_gold["text"] == denotation["text"]
+        real_entities_retrived = 0
+        for denot_gold in dict_gold["denotations"]:
+            span_gold = denot_gold["span"]
+            for denot in denotation["denotations"]
+                if span_gold == denot["span"]:
+                    real_entities_retrived += 1
 
-    return real_entities_retrived, real_entities
+    return real_entities_retrived
+
+
+def find_real_entities(dict_gold):
+    real_entities = 0
+    for denot_gold in dict_gold["denotations"]:
+        span_gold = denot_gold["span"]
+        real_entities += 1
+return real_entities
+
+
+def find_entities_retrived(denot)
+    entities_retrived = 0
+    for span in denot["denotations"][span]:
+        entities_retrived += 1
+return entities_retrived
 
 
 def Recall():
-
-    return
+    recall = real_entities_retrived/real_entities
+    return recall
 
 
 def Precision():
+    precision = real_entities_retrived/entities_retrieved
+    return precision
 
-    return
+
+def main():
+    
 
 
 if __name__ == '__main__':
