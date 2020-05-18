87fb0503bcb3374db711b623f7ba1b5448e457b3
snoboll <jesper.laurell1@gmail.com>
2020-05-18 11:03:56 +0200

    update

--- evaluation.py
+++ evaluation.py
@@ -3,36 +3,6 @@ from os import listdir
 from os.path import isfile, join
 import time
 
-
-def find_real_entities_retrieved(dict_gold, dict_denot, scoredict):
-    cats = list(scoredict.keys())
-    if dict_gold["text"] == dict_denot["text"]:
-        for denot_gold in dict_gold["denotations"]:
-            span_gold = denot_gold["span"]
-            this_cat = denot_gold["id"]
-            if this_cat in cats:
-                for denot in dict_denot["denotations"]:
-                    if span_gold == denot["span"]:
-                        scoredict[this_cat]["real_ent_ret"] += 1
-
-    return scoredict
-
-def find_real_entities(dict_gold, scoredict):
-    cats = list(scoredict.keys())
-    for match_gold in dict_gold["denotations"]:
-        this_cat = match_gold["id"]
-        if this_cat in cats:
-            scoredict[this_cat]["real_ent"] += 1
-    return scoredict
-
-def find_entities_retrieved(dict_denot, scoredict):
-    cats = list(scoredict.keys())
-    for denot in dict_denot["denotations"]:
-        this_cat = denot["id"]
-        if this_cat in cats:
-            scoredict[this_cat]["ent_ret"] += 1
-    return scoredict
-
 def update_scoredict(scoredict, dict_denot, dict_gold):
     cats = list(scoredict.keys())
     for denot in dict_denot["denotations"]:
@@ -40,16 +10,16 @@ def update_scoredict(scoredict, dict_denot, dict_gold):
         if this_cat in cats:
             scoredict[this_cat]["ent_ret"] += 1
             
-            if denot["span"] in dict_gold["denotations"]:
-                scoredict[this_cat]["real_ent_ret"] += 1
+            for gold_denot in dict_gold["denotations"]:
+                if gold_denot["span"] == denot["span"]:
+                    scoredict[this_cat]["real_ent_ret"] += 1
+     
 
     for gold_denot in dict_gold["denotations"]:
         this_cat = gold_denot["id"]
         if this_cat in cats:
             scoredict[this_cat]["real_ent"] += 1
     
-    return scoredict
-
 def get_recall(real_entities, real_entities_retrieved):
     if real_entities == 0:
         recall = '0 real ent'
@@ -81,32 +51,59 @@ def main():
     gold_dicts = get_dicts(gold_folder_path)
 
     #setting up our dicts
-    denot_folder_path = os.path.abspath("gold_papers_tagged") + "/"
+    denot_folder_path = os.path.abspath("out_test") + "/"
     denot_dicts = get_dicts(denot_folder_path)
 
     #setting up cats and dict for separate class scoring
     cats = ["Virus_SARS-CoV-2", "Disease_COVID-19", "Symptom_COVID-19"]
-    entlist = ["real_ent_ret","real_ent","ent_ret"]
+    entlist = ["real_ent_ret", "real_ent", "ent_ret"]
     entdict = dict(zip(entlist, [0 for i in range(len(cats))]))
-    scoredict = dict(zip(cats, [entdict for i in range(len(cats))]))
+    scoredict = dict(zip(cats, [entdict.copy() for i in range(len(cats))]))
 
     #evaluation
     for dict_gold, dict_denot in zip(gold_dicts, denot_dicts):
+        update_scoredict(scoredict, dict_denot, dict_gold)
 
-        scoredict = update_scoredict(scoredict, dict_denot, dict_gold)
-
-    print(str(scoredict) + "\n")
-
+    print(scoredict)
     rec_prec = ["recall", "precision"]
     rec_prec_init = dict(zip(rec_prec, [0 for i in range(len(rec_prec))]))
-    rec_prec_dict = dict(zip(cats, [rec_prec_init for i in range(len(cats))]))
+    rec_prec_dict = dict(zip(cats, [rec_prec_init.copy() for i in range(len(cats))]))
     for cat in scoredict:
         rec_prec_dict[cat]["recall"] = get_recall(scoredict[cat]["real_ent"], scoredict[cat]["real_ent_ret"])
         rec_prec_dict[cat]["precision"] = get_precision(scoredict[cat]["real_ent_ret"], scoredict[cat]["ent_ret"])
-
    
-    #print(rec_prec_dict)
+    print(rec_prec_dict)
+    tot_prec, tot_rec, tot_tp, tot_fp, tot_fn = 0, 0, 0, 0, 0
+    for cat in cats:
+        tot_prec += rec_prec_dict[cat]["precision"]
+        tot_rec += rec_prec_dict[cat]["recall"]
+        tp = scoredict[cat]["real_ent_ret"]
+        fp = scoredict[cat]["ent_ret"] - scoredict[cat]["real_ent_ret"]
+        fn = scoredict[cat]["real_ent"] - scoredict[cat]["real_ent_ret"]
+
+        tot_tp += tp
+        tot_fp += fp
+        tot_fn += fn
+
+        print(cat + " precision: " + str(rec_prec_dict[cat]["precision"]))
+        print(cat + " recall: " + str(rec_prec_dict[cat]["recall"]))
+        print(cat + " true positives: " + str(tp))
+        print(cat + " false positives: " + str(fp))
+        print(cat + " false negatives: " + str(fn))
+
     
+    macro_prec = tot_prec/len(cats)
+    macro_rec = tot_rec/len(cats)
+    micro_prec = tot_tp/(tot_tp+tot_fp)
+    micro_rec = tot_tp/(tot_tp+tot_fn)
+    harmonic_mean = 2*macro_prec*macro_rec/(macro_prec + macro_rec)
+
+    print("macro precision:" + str(macro_prec) + "\n"
+    "macro recall: " + str(macro_rec) + "\n"
+    "micro precision :" + str(micro_prec) + "\n"
+    "micro recall: " + str(micro_rec) + "\n"
+    "harmonic mean: " + str(harmonic_mean))
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -2,11 +2,15 @@ import json
 
 #a = { "denotations": [{"id": "A2", "span": {"begin": 76, "end": 84}, "obj": "Virus_other:694009 "}, {"id": "A3", "span": {"begin": 255, "end": 296}, "obj": "Virus_SARS-CoV-2:2697049"}, {"id": "A1", "span": {"begin": 19, "end": 36}, "obj": "Virus_SARS-CoV-2:2697049"}, {"id": "A6", "span": {"begin": 38, "end": 47}, "obj": "Virus_SARS-CoV-2:2697049"}]}
 
-a = ["Virus_SARS-CoV-2", "Disease_COVID-19", "Symptom_COVID-19"]
-b = ["real_ent_ret","real_ent","ent_ret"]
+a = {'a':{'a':1}, 'b':{'a':5}}
 
-entities = dict(zip(b, [0 for i in range(len(a))]))
-scoredict = dict(zip(a, [entities for i in range(len(a))]))
-print(scoredict)
+def plusa():
+    a['a']['a'] += 1
 
-print(list(scoredict.keys()))
+def plusb():
+    a['b']['a'] += 9
+
+plusa()   
+plusa()
+plusb()
+print(a)
